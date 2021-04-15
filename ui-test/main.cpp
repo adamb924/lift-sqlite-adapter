14f6a6c93ca69f0e0bde61f2e7995682fb2bc8e8
adamb924 <adamb924@gmail.com>
2021-04-15 14:53:19 +0430

    Tool to import LIFT database

--- ui-test/main.cpp
+++ ui-test/main.cpp
@@ -1,14 +1,33 @@
 #include "liftsearchform.h"
 #include "liftdatabase.h"
 #include "datatypes/writingsystem.h"
+#include "liftimporter.h"
 
 #include <QApplication>
 #include <QtDebug>
 #include <QMainWindow>
+#include <QFileInfo>
+#include <QFileDialog>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    /// vaguely cheaty: if the database doesn't exist, offer to import it
+    if( ! QFileInfo::exists("lexicon.sqlite") )
+    {
+        QString fileName = QFileDialog::getOpenFileName(nullptr, QObject::tr("Import LIFT Lexicon"),
+                                                        "",
+                                                        QObject::tr("LIFT Lexicon (*.lift)"));
+        if( fileName.isEmpty() )
+        {
+            return 0;
+        }
+        else
+        {
+            LiftImporter::import( fileName, "lexicon.sqlite" );
+        }
+    }
 
     LiftDatabase db("lexicon.sqlite");
     db.addWritingSystem( WritingSystem("tk-TM") );
